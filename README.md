13cf8849c5dc9915ffcd58053b291738101dfd7b
Kil Do Yeon <37771575+sanha-kil@users.noreply.github.com>
2024-03-05 18:13:41 +0900

    doc: README.md 추가

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # @reket/ui
+
+### REKET UI/UX LIBRARY
+
+## Info
+
+UI/UX Library Based on `React` and `Tailwind CSS`
+
+## Install
+
+```
+npm install @reket/ui
+```
+
+or
+
+```
+yarn add @reket/ui
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import UIProvider from './UIProvider';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <UIProvider>
-      <App />
-    </UIProvider>
-  </React.StrictMode>,
-);
+export * from './UIProvider';
+export * from './components';
