70fcbad927d9bd9822a65d7370f3200719f4d42a
HyeRrin <candy980426@gmail.com>
2025-02-04 19:19:30 +0900

    remove: admin 컴포넌트 제거

--- src/components/dialog/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Modal } from './Modal';
-export { default as Alert } from './Alert';
-export { default as Notification } from './Notification';
-export { default as Tooltip } from './Tooltip';
--- src/components/input/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export { default as TextInput } from './TextInput';
-export { default as DatePicker } from './DatePicker';
-export { default as Dropdown } from './Dropdown';
-export { default as Select } from './Select';
