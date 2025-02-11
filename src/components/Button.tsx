ac6a162e739e0f744388c2330caed0b7a9ecf241
HyeRrin <candy980426@gmail.com>
2025-02-11 18:04:22 +0900

    modify: button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -47,8 +47,8 @@ const SIZE_STYLE = {
   xs: 'px-[8px] h-[28px] text-[11px] font-normal',
   s: 'px-[12px] h-[36px] text-[12px] font-medium',
   m: 'px-[16px] h-[44px] text-[14px] font-medium',
-  l: 'px-[16px] h-[48px] text-[14px] font-medium',
-  xl: 'px-[16px] h-[52px] text-[16px] font-medium',
+  l: 'px-[16px] h-[48px] text-[14px] font-semibold',
+  xl: 'px-[16px] h-[52px] text-[16px] font-bold',
 };
 
 const DISPLAY_STYLE = { block: 'w-full', inline: '' };
@@ -56,18 +56,18 @@ const DISPLAY_STYLE = { block: 'w-full', inline: '' };
 const OPTION_STYLE = {
   dark: {
     filled: 'text-white bg-dark-6 hover:bg-dark-8',
-    outline: 'text-dark-6 border-[1px] border-solid border-dark-6 bg-white hover:bg-dark-1',
+    outline: 'text-dark-6 border-[1px] border-solid border-dark-6 bg-white hover:bg-dark-0',
   },
   purple: {
     filled: 'text-white bg-violet-6 hover:bg-violet-9',
-    outline: 'text-violet-6 border-[1px] border-solid border-violet-6 bg-white hover:bg-violet-1',
+    outline: 'text-violet-6 border-[1px] border-solid border-violet-6 bg-white hover:bg-violet-0',
   },
   red: {
     filled: 'text-white bg-red-6 hover:bg-red-8',
-    outline: 'text-red-6 border-[1px] border-solid border-red-6 bg-white hover:bg-red-1',
+    outline: 'text-red-6 border-[1px] border-solid border-red-6 bg-white hover:bg-red-0',
   },
   gray: {
     filled: 'text-white bg-gray-6 hover:bg-gray-8',
-    outline: 'text-black border-[1px] border-solid border-gray-4 bg-white hover:bg-gray-1',
+    outline: 'text-black border-[1px] border-solid border-gray-4 bg-white hover:bg-gray-0',
   },
 };
