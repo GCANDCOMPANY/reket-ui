3e2e8f33fd2dfed898461d3f0197fe2ce50b94e0
HyeRrin <candy980426@gmail.com>
2025-02-25 15:01:47 +0900

    add: new components

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App';
-// import { UIProvider } from './UIProvider';
-// import './styles/globals.scss';
-
-// const container = document.getElementById('root');
-// if (container)
-//   createRoot(container).render(
-//     <UIProvider>
-//       <App />
-//     </UIProvider>,
-//   );
-
-import './styles/globals.scss';
-
 export * from './components/button';
+export * from './components/dialog';
+export * from './components/element';
 export * from './components/input';
+export * from './components/radio';
+export * from './components/text';
 export * from './UIProvider';
 export * from './hooks';
