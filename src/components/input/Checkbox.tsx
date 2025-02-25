8f8e64ec951c954a7c561e6eba856c99e5670233
HyeRrin <candy980426@gmail.com>
2025-02-25 14:59:32 +0900

    add: Checkbox Component

---
+++ src/components/input/Checkbox.tsx
@@ -0,0 +1,28 @@
+import { IoCheckbox } from '@react-icons/all-files/io5/IoCheckbox';
+import { colors } from '../../constants';
+import { mergeStyle } from '../../utils/style';
+
+interface Props {
+  label?: string;
+  isChecked?: boolean;
+  size?: number;
+  onClick?: () => void;
+  className?: Tailwind.ClassNames;
+}
+
+const Checkbox = ({ label, isChecked = false, size = 18, onClick, className }: Props) => {
+  const containerStyle = mergeStyle('flex items-center cursor-pointer', className);
+
+  return (
+    <button className={containerStyle} onClick={onClick}>
+      <IoCheckbox size={size} color={isChecked ? 'black' : colors.gray[4]} />
+      {!!label && (
+        <p className={`ml-4 tracking-tight ${isChecked ? 'text-dark-6' : 'text-gray-6'}`}>
+          {label}
+        </p>
+      )}
+    </button>
+  );
+};
+
+export default Checkbox;
--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -4,3 +4,4 @@ export { default as InputNumber } from './InputNumber';
 export { default as Select } from './Select';
 export { default as TextArea } from './TextArea';
 export { default as InputLabel } from './InputLabel';
+export { default as Checkbox } from './Checkbox';
