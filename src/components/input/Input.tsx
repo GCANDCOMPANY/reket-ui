e37f175ed2878aea8ba3a966813e430ab1da6249
HyeRrin <candy980426@gmail.com>
2025-03-21 16:20:46 +0900

    modify: Input Component Style

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -29,16 +29,14 @@ const Input = ({
   ...props
 }: Props): JSX.Element => {
   const textSize = `text-${fontSize}`;
-  const containerClass = mergeStyle(`flex items-center `, className);
+  const containerClass = mergeStyle(`flex items-center`, className);
 
   return (
     <div className={containerClass}>
       {!!label && <InputLabel label={label} required={required} />}
       <div className="flex-1">
         <input
-          name={name}
-          value={value}
-          className={`${textSize} ${disabled && 'cursor-not-allowed'} h-[44px] w-full appearance-none rounded border-[1px] border-gray-3 bg-white px-12 font-light placeholder-gray-5 caret-dark-6 focus:border-dark-3 focus:outline-none`}
+          className={`${textSize} ${disabled && 'cursor-not-allowed'} h-[44px] w-full appearance-none rounded border-1 border-gray-3 bg-white px-12 placeholder-gray-5 caret-dark-6 focus:border-dark-3 focus:outline-none`}
           placeholder={placeholder}
           disabled={disabled}
           onChange={(e) => {
